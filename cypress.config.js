e502212830cb4087485498b81ae6239bbc39e67b
ManewJ <manoeljustino@gmail.com>
2026-06-01 22:15:57 -0300

    chore: adiciona arquivos de configuracao e suporte compartilhados

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,35 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  createEsbuildPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: "https://portaldatransparencia.gov.br",
+    specPattern: "cypress/e2e/features/**/*.feature",
+    supportFile: "cypress/support/e2e.js",
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    defaultCommandTimeout: 15000,
+    pageLoadTimeout: 60000,
+    video: false,
+    screenshotOnRunFailure: true,
+    screenshotsFolder: "cypress/screenshots",
+    trashAssetsBeforeRuns: true,
+
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+      return config;
     },
   },
 });
